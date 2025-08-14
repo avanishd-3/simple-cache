c4ecf1615ac7513c1f7a1e72d33581307bcba9b9
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-08-14 12:09:41 -0700

    Respond to ping

--- app/main.py
+++ app/main.py
@@ -5,10 +5,11 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+
+    # Wait for client connection
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
