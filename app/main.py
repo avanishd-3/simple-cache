041b6d2170d069c95753d17cfa355d37204311ca
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-08-14 17:19:25 -0700

    Switch to async event loop for server

--- app/main.py
+++ app/main.py
@@ -1,22 +1,32 @@
-import socket  # noqa: F401
+import asyncio
 
 
-def main():
+async def handle_echo(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+    data = None
+
+    while data != b"QUIT\r\n":
+        data = await reader.read(1024)
+        
+        if not data:
+            break
+
+        writer.write(b"+PONG\r\n")
+        await writer.drain() # Flush write buffer
+
+    writer.close()
+    await writer.wait_closed()
+
+
+
+async def main() -> None:
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server = await asyncio.start_server(handle_echo, "localhost", 6379) # Client function called whenever client sends a message
 
-    # Wait for client connection
-    connection, _ = server_socket.accept()
-
-    # Loop until client closes connection
-    while True:
-        data: bytes = connection.recv(1024)
-        if not data:
-            break
-        connection.sendall(b"+PONG\r\n")
-
+    async with server:
+        await server.serve_forever()
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
