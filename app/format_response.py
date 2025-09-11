335abc286a10042f6378d879dc5b5f8d0cc26a9b
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-09-10 18:50:56 -0700

    docs: clarify that format response follows RESP

--- app/format_response.py
+++ app/format_response.py
@@ -1,12 +1,12 @@
 def format_simple_string(message: str) -> bytes:
     """
-    Format a simple Redis string.
+    Format a simple RESP string.
     """
     return f"+{message}\r\n".encode("utf-8")
 
 def format_bulk_string_success(message: str) -> bytes:
     """
-    Format a Redis bulk success response.
+    Format a RESP bulk success response.
     """
     return f"${len(message)}\r\n{message}\r\n".encode("utf-8")
 
@@ -29,12 +29,12 @@ def format_resp_array(elements: list[str]) -> bytes:
 
 def format_null_bulk_string() -> bytes:
     """
-    Format a null bulk string Redis response.
+    Format a null bulk string RESP response.
     """
     return "$-1\r\n".encode("utf-8")
 
 def format_simple_error(message: str) -> bytes:
     """
-    Format a simple Redis error response.
+    Format a simple RESP error response.
     """
     return f"-{message}\r\n".encode("utf-8")
